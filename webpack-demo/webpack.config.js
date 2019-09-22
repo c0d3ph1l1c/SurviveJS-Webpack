0f1acf0e1b4e5db7b62b59371390b8c4e1698932
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2019-09-22 19:07:52 +0800

    Developing/webpack-dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,25 @@
+const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    devServer: {
+        stats: "errors-only",
+        host: process.env.HOST,
+        port: process.env.PORT,
+        open: true,
+        overlay: true,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000,
+        }
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack demo",
         }),
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, "node_modules")
+        ]),
     ],
 };
