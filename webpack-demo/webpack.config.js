2eee3cf817c26a0d7f2dd1dd1550c21d76fe57fe
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2019-09-23 16:39:10 +0800

    Styling/Separating CSS/globbing

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,11 +2,16 @@ const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
 
 const parts = require("./webpack.parts");
 
 const commonConfig = merge([
     {
+        entry: {
+            main: './src/index.js',
+            style: glob.sync("./src/**/*.css"),
+        },
         plugins: [
             new HtmlWebpackPlugin({
                 title: "Webpack demo",
